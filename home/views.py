2fe11460b67ef08107ba52fd2c01c7607457ea25
Chekwube Utomi <chekwubeutomi@gmail.com>
2023-07-23 17:23:18 -0700

    initial commit

--- home/views.py
+++ home/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 def home(request):
     context = {'title': 'Home | SSCEPlus - Suitable for Senior Secondary School Students Preparing for Certificate Examinations.'}
     return render(request, 'home\index.html', context)
+
+def subjects(request):
+    context = {'title': 'Subjects | SSCEPlus - Suitable for Senior Secondary School Students Preparing for Certificate Examinations.'}
+    return render(request, 'home\subjects.html', context)
