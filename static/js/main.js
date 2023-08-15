777cb8d666ecfb038dac0276f716c80462790a91
Chekwube Utomi <chekwubeutomi@gmail.com>
2023-08-15 07:42:31 -0700

    carousal implemented with vinilla js

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,39 @@
 const toggle = document.getElementById('toggle')
 const navbar = document.getElementById('navbar')
+const next = document.getElementById('next')
+const previous = document.getElementById('previous')
 
 toggle.addEventListener('click', () =>{
     navbar.classList.toggle('active')
 })
+
+const slides = document.querySelectorAll('.slide')
+
+slides.forEach(node=>{
+    console.log(node.firstElementChild.innerText)
+    let position = Number(node.firstElementChild.innerText) * 105
+    node.firstElementChild.innerText = position
+
+    node.style.transform = `translate(${position}%, 0)`
+    
+    console.log(position)
+  
+})
+
+let curr = 0
+
+next.addEventListener('click', ()=>{
+    curr = curr + 105
+    slides.forEach(node=>{
+        let position = Number(node.firstElementChild.innerText) - 105
+        if (position < 0){
+            position = 315
+        }
+        node.firstElementChild.innerText = position
+        node.style.transform = `translate(${position}%, 0)`
+
+    })
+
+})
+
+console.log(slides)
