a795a63146aa189ce0c187c0caf7622748851bb8
cheks123 <chekwubeutomi@gmail.com>
2024-01-29 05:37:27 -0800

    subject text added

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,5 @@
 const toggle = document.getElementById('toggle')
 const navbar = document.getElementById('navbar')
-const next = document.getElementById('next')
-const previous = document.getElementById('previous')
 
 toggle.addEventListener('click', () =>{
     navbar.classList.toggle('active')
